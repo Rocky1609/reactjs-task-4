3a762a1f27db684a3ba54ce01a1a4a281b8270ef
Rocky1609 <sairakeh419@gmail.com>
2023-10-13 13:17:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useState,useEffect } from "react";
+import Axios from "axios"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const url ="https://dummyjson.com/users";
+  const [arr,setArr] =useState([]);
+  
+  useEffect(()=>{
+    Axios.get(url)
+    .then((res)=>setArr(res.data.users))
+    .catch((err)=>console.log(err))
+  },[])
+
+  return(
+    <div className="dummy-data-container">
+      <h1 className="dummy-data-heading">Dummy data</h1>
+      <table cellSpacing={0} className="dummy-data-table">
+        <tr>
+          <th>Sno</th>
+          <th>Profile Pic</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Gender</th>
+          <th>E-mail</th>
+          <th>Username</th>
+          <th>Domain</th>
+          <th>IP</th>
+          <th>University</th>
+        </tr>
+        {arr.map((user)=>
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td ><img src={user.image} alt={user.id} className="profile"/></td>
+          <td>{user.firstName}</td>
+          <td>{user.lastName}</td>
+          <td>{user.gender}</td>
+          <td>{user.email}</td>
+          <td>{user.username}</td>
+          <td>{user.domain}</td>
+          <td>{user.ip}</td>
+          <td>{user.university}</td>
+        </tr>
+        )}
+      
+    
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+export default App
